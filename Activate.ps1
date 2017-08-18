fd698bb6003d4def149ca471210e8df737cc71ee
Jhett Black <jhettblack@hotmail.com>
2017-08-18 19:47:35 +1000

    Launch a GUI version of Unity first to prevent license activation timeout.

--- Activate.ps1
+++ Activate.ps1
@@ -1,11 +1,42 @@
 ﻿# Config
 $licensesFile = "unityLicenses.csv";
 $unityPath = "D:/Unity/Editor/Unity.exe";
+$dummyLogTime = 5;
+$dummyPostInitTime = 10;
 
 $scriptPath = $MyInvocation.MyCommand.Path;
 $dir = Split-Path $scriptPath;
 
 cd $dir;
+
+# HACK!!
+# We first load Unity without activating due to an issue where activation will fail if we immediately start from the command line.
+# This process is killed after we detect "Command Line Arguments" in the editor log, as this indicates Unity has loaded.
+$dummyLogPath = "$pwd\dummy.log";
+Remove-Item $dummyLogPath;
+$dummyProc = Start-Process $unityPath -ArgumentList "-logFile `"$dummyLogPath`"" -NoNewWindow -PassThru;
+
+$dummyLoaded = $false;
+while(($dummyLoaded -eq $false))
+{
+    Start-Sleep -s $dummyLogTime;
+
+    $dummyLogData = Get-Content $dummyLogPath;
+
+    foreach ($line in $dummyLogData)
+    {
+        if($line -like "*COMMAND LINE ARGUMENTS*")
+        {
+			# Unity has initialized. Give it a bit more time to reach the license screen before terminating.
+			Start-Sleep -s $dummyPostInitTime;
+            $dummyLoaded = $true;
+            break;
+        }
+    }
+}
+
+Stop-Process -InputObject $dummyProc;
+Wait-Process -InputObject $dummyProc;
 
 # Add our licenses to the available pool for elastic agents.
 $licenseCSV = @(Import-Csv $licensesFile);
